3eb8567f76c8f2a6996c8f46bcc4f1537f8b2be0
Mikhail Sidenkov <mikhail.sidenkov@bytepace.com>
2021-11-20 00:10:58 +0600

    add Info module

--- TestTask/Screens/Info/InfoView.swift
+++ TestTask/Screens/Info/InfoView.swift
@@ -5,4 +5,25 @@
 //  Created by mage on 19.11.2021.
 //
 
-import Foundation
+import UIKit
+import PinLayout
+
+class InfoView: UIView {
+    init() {
+        super.init(frame: .zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    let tableView = UITableView()
+    
+    override func layoutSubviews() {
+        tableView.pin.all(pin.safeArea)
+    }
+}
+
+extension InfoView {
+    
+}
--- TestTask/Screens/Info/InfoViewController.swift
+++ TestTask/Screens/Info/InfoViewController.swift
@@ -5,4 +5,24 @@
 //  Created by mage on 20.11.2021.
 //
 
-import Foundation
+import UIKit
+
+class InfoViewController: UIViewController {
+    let _view = InfoView()
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+//        setupBindings()
+    
+//        tabBarItem = UITabBarItem(title: nil, image: UIImage(named: "menu_history_item_new"),
+//                                  selectedImage: UIImage(named: "menu_history_item_new"))
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func loadView() {
+        view = _view
+    }
+}
