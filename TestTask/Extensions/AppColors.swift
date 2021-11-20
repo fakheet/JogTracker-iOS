6cf1cff92f86b12dd0d48a5d3e9acaf4e4a83658
Mikhail Sidenkov <mikhail.sidenkov@bytepace.com>
2021-11-20 21:31:34 +0600

    add more colors

--- TestTask/Extensions/AppColors.swift
+++ TestTask/Extensions/AppColors.swift
@@ -12,9 +12,13 @@ extension UIColor {
     static let appDarkGreen = UIColor(red: 101, green: 157, blue: 44)
     
     static let appPurple = UIColor(red: 219, green: 140, blue: 243)
-    
+
+    static let appWhite2 = UIColor(red: 234, green: 234, blue: 234)
+    static let appWhite3 = UIColor(red: 215, green: 215, blue: 215)
     static let appGrey = UIColor(red: 166, green: 161, blue: 161)
+    static let appWarmGrey = UIColor(red: 151, green: 151, blue: 151)
     static let appDarkGrey = UIColor(red: 61, green: 59, blue: 62)
+    static let appGreyish = UIColor(red: 176, green: 171, blue: 171)
 
     convenience init(red: Int, green: Int, blue: Int, a: Int = 255) {
         self.init(
