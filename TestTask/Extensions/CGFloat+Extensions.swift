758e43f96e8c87871414d2c947a0f487a8f596a8
Mikhail Sidenkov <mikhail.sidenkov@bytepace.com>
2021-11-20 21:31:43 +0600

    add more margins

--- TestTask/Extensions/CGFloat+Extensions.swift
+++ TestTask/Extensions/CGFloat+Extensions.swift
@@ -5,10 +5,13 @@
 //  Created by mage on 20.11.2021.
 //
 
-import CoreGraphics
+import UIKit
 
 extension CGFloat {
     static let marginSmall: CGFloat = 8
-    static let margin: CGFloat = 24
+    static let margin: CGFloat = 16
+    static let marginMedium: CGFloat = 24
     static let marginLarge: CGFloat = 48
+
+    static let onePixel: CGFloat = 1 / UIScreen.main.scale
 }
